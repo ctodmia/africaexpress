494da29ebb75575dd560538272b289a790066bf0
Carine TOdmia <ctodmia@gmail.com>
2016-01-13 15:53:39 -0800

    completed homepage design

--- client/controllers/sellcontroller.js
+++ client/controllers/sellcontroller.js
@@ -2,3 +2,10 @@ angular.module('camerXpress')
 	.controller('SellController', function($scope){
 		console.log('this is the sell controller')
 	})
+
+	//on the sell view we want 
+		//collect the name of the seller
+		//item picture,
+		//item name, 
+		//item price, 
+		

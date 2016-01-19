2b0cafdf6e3b8f0610a2381afcdf273840d6a4ab
Carine TOdmia <ctodmia@gmail.com>
2016-01-18 20:08:59 -0800

    checking database connection

--- server/model/item.js
+++ server/model/item.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 // var mongodbUri = 'mongodb://test:test@ds045785.mongolab.com:45785/africaexpress'
 var Schema = mongoose.Schema;
 
-var db = mongoose.createConnection('mongodb://test:test@ds045785.mongolab.com:45785/africaexpress')
+var db = mongoose.createConnection('mongodb://test:test@ds045021.mongolab.com:45021/heroku_h86nqkc7')
 // var db = mongoose.connection;
 
 db.on('error', function(err){
